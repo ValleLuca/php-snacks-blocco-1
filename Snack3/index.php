4f99db751883a4cfd88117a0d45d815901e4d21f
ValleLuca <valle.lucaAb@outlook.it>
2022-02-15 19:03:46 +0100

    Snack3 concluso

---
+++ Snack3/index.php
@@ -0,0 +1,30 @@
+<?php
+
+    $randomNum = [];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack C</title>
+</head>
+<body>
+    
+    <?php
+        for($i = 0; count($randomNum) < 15; $i++){ 
+            $numeri = rand(1, 120);
+            if(!in_array($numeri, $randomNum)){
+                $randomNum[] =  $numeri;
+            }
+            echo $randomNum[$i];
+            echo " ";
+        };
+        
+    ?>
+
+</body>
+</html>
